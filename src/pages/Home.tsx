cb9129d03871fbbca42e1c88aabb9d424461eb1c
Akash <akabhowmick@gmail.com>
2024-12-30 20:36:03 -0600

    add home page

---
+++ src/pages/Home.tsx
@@ -0,0 +1,10 @@
+const Home: React.FC = () => {
+  return (
+    <div className="flex flex-col items-center justify-center h-screen bg-gray-100">
+      <h1 className="text-4xl font-bold">Welcome to the Taekwondo App</h1>
+      <p className="mt-4 text-lg text-gray-600">Stay connected with your community!</p>
+    </div>
+  );
+};
+
+export default Home;
