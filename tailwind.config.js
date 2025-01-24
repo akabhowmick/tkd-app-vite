607ad7c83d1d9d5b1f8958082b2fbe0bf5287da2
Akash <akabhowmick@gmail.com>
2025-01-24 10:47:20 -0500

    home page animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        bg: "floating 5s infinite ease-in-out",
+      },
+      keyframes: {
+        floating: {
+          "0%, 100%": { transform: "translateY(0) rotate(0deg)" },
+          "50%": { transform: "translateY(-20px) rotate(10deg)" },
+        },
+      },
+    },
   },
   plugins: [],
-}
+};
